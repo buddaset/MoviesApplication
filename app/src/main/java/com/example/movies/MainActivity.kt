952ef7ca1ab422bff784bb71ae2c4a3620d77b19
BuddaSet <buddaset@mail.ru>
2022-04-11 19:45:54 +0300

    add fragment detailMovie add fragment listMovie create transaction listMovie -> detailMovie

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -5,19 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movies.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MoviesListFragment.ClickMovieListener {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        binding.openDetail.setOnClickListener { openMovieDetailActivity() }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container_fragment, MoviesListFragment.newInstance() )
+                .commit()
+        }
+
 
     }
 
-    private fun openMovieDetailActivity() {
-        startActivity(Intent(this, MovieDetailsActivity::class.java))
+    override fun clickMovie() {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.container_fragment, MoviesDetailsFragment.newInstance())
+            .commit()
     }
 
 
