e876a9b3663530373d66f0569129a7fd614d7d6a
BuddaSet <buddaset@mail.ru>
2022-04-28 05:08:33 +0300

    add annotation for  MovieDetailsResponse.kt + change field names

--- app/src/main/java/com/example/movies/data/remote/response/MovieDetailsResponse.kt
+++ app/src/main/java/com/example/movies/data/remote/response/MovieDetailsResponse.kt
@@ -1,26 +1,17 @@
 package com.example.movies.data.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieDetailsResponse(
-    val adult: Boolean,
-    val backdrop_path: String,
-    val belongs_to_collection: Any,
-    val budget: Int,
-    val genres: List<GenreResponse>,
-    val homepage: String,
-    val id: Int,
-    val imdb_id: String,
-    val original_language: String,
-    val original_title: String,
-    val overview: String,
-    val popularity: Double,
-    val poster_path: String,
-    val release_date: String,
-    val revenue: Int,
-    val runtime: Int,
-    val status: String,
-    val tagline: String,
-    val title: String,
-    val video: Boolean,
-    val vote_average: Double,
-    val vote_count: Int
-)
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("adult") val adult: Boolean,
+    @SerializedName("backdrop_path") val imageDatailPath: String,
+    @SerializedName("runtime") val runningTime: Int,
+    @SerializedName("vote_average") val rating: Double,
+    @SerializedName("vote_count")  val reviewCount: Int,
+    @SerializedName("overview") val storyLine: String,
+    @SerializedName("genres") val genres: List<GenreResponse>,
+    @SerializedName(" release_date") val release_date: String,
+
+    )
